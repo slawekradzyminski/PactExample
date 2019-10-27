c2a18f763cc428e40b3e055303f9324ab5f6ce5a
slawekradzyminski <slawomir.radzyminski@gmail.com>
2019-10-27 08:24:54 +0100

    added swagger

--- example-consumer-ronaldo/src/main/java/com/awesome/testing/Ronaldo.java
+++ example-consumer-ronaldo/src/main/java/com/awesome/testing/Ronaldo.java
@@ -2,9 +2,25 @@ package com.awesome.testing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
 public class Ronaldo {
+
+    @Bean
+    public Docket swagger() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Ronaldo.class, args);
